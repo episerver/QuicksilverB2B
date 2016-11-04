c6e1ce31e78c4a34f1aecebf5c7387f4a8db7db0
AlecsandruTiponut <alecsandru.tiponut@techromix.com>
2016-11-04 13:14:04 +0200

    Added Metadata and Metadata plus initializations

---
+++ Sources/EPiServer.Reference.Commerce.Site/Infrastructure/InitializationModules/MetadataInitialization.cs
@@ -0,0 +1,48 @@
+﻿using System.Linq;
+using EPiServer.Framework;
+using EPiServer.Framework.Initialization;
+using Mediachase.BusinessFoundation.Data;
+using Mediachase.BusinessFoundation.Data.Meta.Management;
+using Mediachase.BusinessFoundation.MetaForm;
+
+namespace EPiServer.Reference.Commerce.Site.Infrastructure.InitializationModules
+{
+    [InitializableModule]
+    [ModuleDependency(typeof(EPiServer.Commerce.Initialization.InitializationModule))]
+    public class MetadataInitialization : IInitializableModule
+    {
+        public void Initialize(InitializationEngine context)
+        {
+            InitializeCustomerMetaData();
+        }
+
+        private void InitializeCustomerMetaData()
+        {
+            //var customerMetadata =
+            //    DataContext.Current.MetaModel.MetaClasses.Cast<MetaClass>()
+            //        .First(mc => mc.Name == metaClassName);
+            //CreateMetaField(customerMetadata, metaFieldName, MetaFieldType.Integer);
+        }
+
+        private void CreateMetaField(MetaClass metaClass, string metaFieldName, string type)
+        {
+            if (metaClass.Fields[metaFieldName] == null)
+            {
+                metaClass.CreateMetaField(metaFieldName, metaFieldName, type, true, "", new AttributeCollection());
+                FormController.AddMetaPrimitive(metaClass.Name, Forms.EditForm, metaFieldName);
+            }
+        }
+
+        public void Uninitialize(InitializationEngine context)
+        {
+            //Add uninitialization logic
+        }
+    }
+
+    public class Forms
+    {
+        public const string EditForm = "[MC_BaseForm]";
+        public const string ShortInfoForm = "[MC_ShortViewForm]";
+        public const string ViewForm = "[MC_GeneralViewForm]";
+    }
+}

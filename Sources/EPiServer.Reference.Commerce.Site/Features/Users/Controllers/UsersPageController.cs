e3dfd8ba1eafcf183bcaf395fe3b1231c36dcc1b
Marius Lazar <marius.lazar@luminoslabs.com>
2016-11-07 18:26:13 +0200

    EQB-23 - Added UsersPageController, UsersPage and UsersPageViewModel and the corresponding views for listing, adding and editing actions

---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Users/Controllers/UsersPageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EPiServer.Reference.Commerce.Site.Features.Users.Pages;
+using EPiServer.Reference.Commerce.Site.Features.Users.ViewModels;
+using EPiServer.Web.Mvc;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Users.Controllers
+{
+    [Authorize]
+    public class UsersPageController : PageController<UsersPage>
+    {
+        public ActionResult Index(UsersPage currentPage)
+        {
+            var viewModel = new UsersPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+        public ActionResult AddUser(UsersPage currentPage)
+        {
+            var viewModel = new UsersPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+        public ActionResult Edit(UsersPage currentPage)
+        {
+            var viewModel = new UsersPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Users/ViewModels/UsersPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Reference.Commerce.Site.Features.Shared.ViewModels;
+using EPiServer.Reference.Commerce.Site.Features.Users.Pages;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Users.ViewModels
+{
+    public class UsersPageViewModel : PageViewModel<UsersPage>
+    {
+    }
+}

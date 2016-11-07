69ff799484b930da2daeb2d2ec00f25a01f5544e
Marius Lazar <marius.lazar@luminoslabs.com>
2016-11-07 14:33:26 +0200

    EQB-29 - wf3.0 finished

---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/SubOrganization/Controllers/SubOrganizationPageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EPiServer.Reference.Commerce.Site.Features.Suborganization.Pages;
+using EPiServer.Reference.Commerce.Site.Features.Suborganization.ViewModels;
+using EPiServer.Web.Mvc;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Suborganization.Controllers
+{
+    [Authorize]
+    public class SubOrganizationPageController : PageController<SubOrganizationPage>
+    {
+        public ActionResult Index(SubOrganizationPage currentPage)
+        {
+            var viewModel = new SubOrganizationPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+        public ActionResult Edit(SubOrganizationPage currentPage)
+        {
+            var viewModel = new SubOrganizationPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+        public ActionResult AddSub(SubOrganizationPage currentPage)
+        {
+            var viewModel = new SubOrganizationPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+    }
+}
---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/SubOrganization/ViewModels/SubOrganizationPageViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using EPiServer.Reference.Commerce.Site.Features.Shared.ViewModels;
+using EPiServer.Reference.Commerce.Site.Features.Suborganization.Pages;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Suborganization.ViewModels
+{
+    public class SubOrganizationPageViewModel : PageViewModel<SubOrganizationPage>
+    {
+    }
+}

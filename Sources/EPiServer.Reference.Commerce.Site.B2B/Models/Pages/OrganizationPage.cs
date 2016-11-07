a6c8693074d49caef1a1493f332a6b8da61bda59
AlecsandruTiponut <alecsandru.tiponut@techromix.com>
2016-11-07 14:46:44 +0200

    Updated Organization model

--- Sources/EPiServer.Reference.Commerce.Site.B2B/Models/Pages/OrganizationPage.cs
+++ Sources/EPiServer.Reference.Commerce.Site.B2B/Models/Pages/OrganizationPage.cs
@@ -5,9 +5,9 @@ using EPiServer.DataAbstraction;
 using EPiServer.DataAnnotations;
 using EPiServer.SpecializedProperties;
 
-namespace EPiServer.Reference.Commerce.Site.Features.Organization.Pages
+namespace EPiServer.Reference.Commerce.Site.B2B.Models.Pages
 {
-    [ContentType(DisplayName = "OrganizationPage", GUID = "e50f0e69-0851-40dc-b00c-38f0acec3f32", Description = "", AvailableInEditMode = false)]
+    [ContentType(DisplayName = "OrganizationPage", GUID = "e50f0e69-0851-40dc-b00c-38f0acec3f32", Description = "", AvailableInEditMode = true)]
     public class OrganizationPage : PageData
     {
 

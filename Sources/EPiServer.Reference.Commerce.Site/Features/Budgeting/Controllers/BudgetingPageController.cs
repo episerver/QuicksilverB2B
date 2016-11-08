730c91bb7e82f152d52104781771939d0591f270
Marius Lazar <marius.lazar@luminoslabs.com>
2016-11-08 11:52:09 +0200

    EQB-24 added budgeting pages

---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Budgeting/Controllers/BudgetingPageController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using EPiServer.Reference.Commerce.Site.Features.Budgeting.Pages;
+using EPiServer.Reference.Commerce.Site.Features.Budgeting.ViewModels;
+using EPiServer.Web.Mvc;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Budgeting.Controllers
+{
+    [Authorize]
+    public class BudgetingPageController : PageController<BudgetingPage>
+    {
+        public ActionResult Index(BudgetingPage currentPage)
+        {
+            var viewModel = new BudgetingPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+        public ActionResult AddBudget(BudgetingPage currentPage)
+        {
+            var viewModel = new BudgetingPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+        public ActionResult EditBudget(BudgetingPage currentPage)
+        {
+            var viewModel = new BudgetingPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+    }
+}

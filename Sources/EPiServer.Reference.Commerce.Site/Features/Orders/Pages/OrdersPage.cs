bc320036ba48baa81266d83f7bcee61e89a80a4b
Marius Lazar <marius.lazar@luminoslabs.com>
2016-11-08 11:24:38 +0200

    added orders page type

---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Orders/Pages/OrdersPage.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using EPiServer.Core;
+using EPiServer.DataAbstraction;
+using EPiServer.DataAnnotations;
+using EPiServer.SpecializedProperties;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Orders.Pages
+{
+    [ContentType(DisplayName = "OrdersPage", GUID = "3c436a14-38d1-4fd1-ab37-15f0848cfa24", Description = "", AvailableInEditMode = false)]
+    public class OrdersPage : PageData
+    {
+        [CultureSpecific]
+        [Display(
+            Name = "Main body",
+            Description = "The main body will be shown in the main content area of the page, using the XHTML-editor you can insert for example text, images and tables.",
+            GroupName = SystemTabNames.Content,
+            Order = 1)]
+        public virtual XhtmlString MainBody { get; set; }
+    }
+}

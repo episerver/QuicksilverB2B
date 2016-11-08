9cd5af5767c4e09c2e7bdcc4e74f4883165e875d
Ionut <ionut.iancau@luminoslabs.com>
2016-11-08 11:46:13 +0200

    fix

--- Sources/EPiServer.Reference.Commerce.Site/Features/Orders/ViewModels/OrdersPageViewModel.cs
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Orders/ViewModels/OrdersPageViewModel.cs
@@ -1,7 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using EPiServer.Reference.Commerce.Site.Features.Orders.Pages;
+using EPiServer.Reference.Commerce.Site.Features.Shared.ViewModels;
 
 namespace EPiServer.Reference.Commerce.Site.Features.Orders.ViewModels
 {

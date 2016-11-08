258ee9176d9b2b66fe34c0cb1b1ece36a7d1a091
Ionut <ionut.iancau@luminoslabs.com>
2016-11-08 11:21:03 +0200

    Incomplete

---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Orders/Controllers/OrdersPageController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace EPiServer.Reference.Commerce.Site.Features.Orders
+{ 
+    [Authorize]
+    public class OrdersPageController : PageController<OrdersPage>
+    {
+        public ActionResult Index(OrdersPage currentPage)
+        {
+            var viewModel = new OrdersPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+        public ActionResult Edit(OrdersPage currentPage)
+        {
+            var viewModel = new OrdersPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+        public ActionResult AddSub(OrdersPage currentPage)
+        {
+            var viewModel = new OrdersPageViewModel { CurrentPage = currentPage };
+            return View(viewModel);
+        }
+    }
+}

57c6ba37711cf362c0e15843e8c7ee98c431de45
bcupc <monagherghel@yahoo.it>
2016-11-16 14:12:50 +0200

    EQB-77 [Organization] Users tab should not be visible if no Organization is added

--- Sources/EPiServer.Reference.Commerce.Site/Features/Navigation/Controllers/B2BNavigationController.cs
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Navigation/Controllers/B2BNavigationController.cs
@@ -3,16 +3,19 @@ using EPiServer.Reference.Commerce.Site.Features.Navigation.ViewModels;
 using EPiServer.Reference.Commerce.Site.Features.Start.Pages;
 using EPiServer.SpecializedProperties;
 using System.Web.Mvc;
+using EPiServer.Reference.Commerce.Site.B2B.ServiceContracts;
 
 namespace EPiServer.Reference.Commerce.Site.Features.Navigation.Controllers
 {
     public class B2BNavigationController : Controller
     {
         private readonly IContentLoader _contentLoader;
+        private readonly IOrganizationService _organizationService;
 
-        public B2BNavigationController(IContentLoader contentLoader)
+        public B2BNavigationController(IContentLoader contentLoader, IOrganizationService organizationService)
         {
             _contentLoader = contentLoader;
+            _organizationService = organizationService;
         }
 
         public ActionResult Index(IContent currentContent)
@@ -25,6 +28,12 @@ namespace EPiServer.Reference.Commerce.Site.Features.Navigation.Controllers
                 CurrentContentLink = currentContent?.ContentLink,
                 UserLinks = new LinkItemCollection()
             };
+
+            var organization = _organizationService.GetCurrentUserOrganization();
+            if (organization == null)
+            {
+                return PartialView(viewModel);
+            }
 
             if (startPage.B2BMenu != null)
             {

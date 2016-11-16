a772269d91ced567cb7c68edaf2d990ceb77dc75
bcupc <monagherghel@yahoo.it>
2016-11-16 18:04:11 +0200

    ServiceConfiguration IQuickOrderService

--- Sources/EPiServer.Reference.Commerce.Site.B2B/Services/QuickOrderService.cs
+++ Sources/EPiServer.Reference.Commerce.Site.B2B/Services/QuickOrderService.cs
@@ -1,7 +1,9 @@
 ﻿using EPiServer.Reference.Commerce.Site.B2B.ServiceContracts;
+using EPiServer.ServiceLocation;
 
 namespace EPiServer.Reference.Commerce.Site.B2B.Services
 {
+    [ServiceConfiguration(typeof(IQuickOrderService), Lifecycle = ServiceInstanceScope.Singleton)]
     public class QuickOrderService : IQuickOrderService
     {
     }

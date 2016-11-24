e598441166ca3b953cfa862d53bb371deb410cea
bcupc <monagherghel@yahoo.it>
2016-11-24 12:04:48 +0200

    EQB-91 WIP

---
+++ Sources/EPiServer.Reference.Commerce.Site/Features/Payment/PaymentMethods/PersonalBudgetPaymentMethod.cs
@@ -0,0 +1,49 @@
+﻿//using EPiServer.Commerce.Order;
+//using EPiServer.Framework.Localization;
+//using EPiServer.ServiceLocation;
+//using Mediachase.Commerce.Orders;
+
+//namespace EPiServer.Reference.Commerce.Site.Features.Payment.PaymentMethods
+//{
+//    public class PersonalBudgetPaymentMethod : PaymentMethodBase
+//    {
+//        private readonly IOrderFactory _orderFactory;
+
+//        public PersonalBudgetPaymentMethod()
+//            : this(LocalizationService.Current, ServiceLocator.Current.GetInstance<IOrderFactory>())
+//        {
+//        }
+
+//        public PersonalBudgetPaymentMethod(IOrderFactory orderFactory)
+//            : this(LocalizationService.Current, orderFactory)
+//        {
+//        }
+
+//        public PersonalBudgetPaymentMethod(LocalizationService localizationService, IOrderFactory orderFactory)
+//            : base(localizationService)
+//        {
+//            _orderFactory = orderFactory;
+//        }
+
+//        public override bool ValidateData()
+//        {
+//            return true;
+//        }
+        
+//        public override IPayment CreatePayment(decimal amount)
+//        {
+//            var payment = _orderFactory.CreatePayment();
+//            payment.PaymentMethodId = PaymentMethodId;
+//            payment.PaymentMethodName = "PersonalBudget";
+//            payment.Amount = amount;
+//            payment.Status = PaymentStatus.Pending.ToString();
+//            payment.TransactionType = TransactionType.Authorization.ToString();
+//            return payment;
+//        }
+
+//        public override void PostProcess(IPayment payment)
+//        {
+//            payment.Status = PaymentStatus.Processed.ToString();
+//        }
+//    }
+//}
